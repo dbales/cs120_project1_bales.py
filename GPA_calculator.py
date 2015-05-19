a82201b679830926c6db1aee55000b1e490446bc
dbales <derrick.bales@protonmail.ch>
2015-05-18 23:19:34 -0400

    Update GPA_calculator.py

--- GPA_calculator.py
+++ GPA_calculator.py
@@ -1,1 +1,27 @@
+print
+print('         XXX PYTHON   GPA   CALCULATOR XXX      ')
+print
+print
+#function that gets the input and spits out output
 
+A = 4
+B = 3
+C = 2
+D = 1
+F = 0 
+
+
+def class1():
+class1 = raw_input('What is the title of the class?:  ')
+creditHr1 = int(raw_input('And how many credit hours  was this course?:  '))
+grade1 =raw_input('and what grade did you earn?: ')
+
+def class2():
+class2 = raw_input('What is the title of the class?')
+creditHr2 = int(raw_input('And how many credit hours  was this course?'))
+grade2 =raw_input('and what grade did you earn?')
+
+def class3():
+class3 = raw_input('What is the title of the class?')
+creditHr3 = int(raw_input('And how many credit hours  was this course?'))
+grade3 =raw_input('and what grade did you earn?')
